eb738f5ec0ebbf118a52b3648be3a7e2131515f6
Vadim <folent1@yandex.ru>
2023-01-22 01:02:05 +0300

    feat: add websocket server

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 import { httpServer } from "./src/http_server/index.js";
-import { mouse } from "@nut-tree/nut-js";
+import { WebSocketServer } from "ws";
 
 const HTTP_PORT = 8181;
+const WS_PORT = 8080;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({ port: WS_PORT });
+
+wss.on('connection', function connection(ws, req) {
+    console.log('CONNECTION_ACCEPTED', req.socket.address().address, req.socket.address().port)
+  ws.on('message', data => {
+    console.log('received: %s', data);
+  });
+});
+
+wss.on('close', () => {
+    console.log('close')
+})
+
+wss.on('error', () => {
+    console.log('error')
+})     
